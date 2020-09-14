3789376fc623636c9f25ab36be2540c26c948504
Tales Ferreira <talesmateus1999@hotmail.com>
2020-09-13 21:44:08 -0400

    Swagger global messages personalizated.

--- src/main/java/com/pml/config/SwaggerConfig.java
+++ src/main/java/com/pml/config/SwaggerConfig.java
@@ -1,10 +1,18 @@
 package com.pml.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,16 +20,48 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+	private final ResponseMessage m200 = simpleMessage(200, "Ok");
+	private final ResponseMessage m201 = simpleMessage(201, "Created");
+	private final ResponseMessage m204put = simpleMessage(204, "Atualização ok");
+	private final ResponseMessage m204del = simpleMessage(204, "Ok");
+	private final ResponseMessage m401 = simpleMessage(401, "Unauthorized");
+	private final ResponseMessage m403 = simpleMessage(403, "Forbidden");
+	private final ResponseMessage m404 = simpleMessage(404, "Not Found");
+	private final ResponseMessage m422 = simpleMessage(422, "Validation error");	
+	private final ResponseMessage m500 = simpleMessage(500, "Unexpected error");
 	
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.any())
-			.paths(PathSelectors.any())
-			.build();
+				.useDefaultResponseMessages(false)
+				.globalResponseMessage(RequestMethod.GET, Arrays.asList(m200, m401, m403, m404, m500))
+				.globalResponseMessage(RequestMethod.POST, Arrays.asList(m201, m401, m403, m422, m500))
+				.globalResponseMessage(RequestMethod.PUT, Arrays.asList(m204put, m401, m403, m404, m422, m500))
+				.globalResponseMessage(RequestMethod.DELETE, Arrays.asList(m204del, m401, m403, m404, m500))
+				.select()
+				.apis(RequestHandlerSelectors
+				.basePackage("com.pml.resources"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"API do curso Spring Boot",
+				"Esta API é utilizada no curso de Spring Boot do prof. Nelio Alves", "Versão 1.0",
+				"https://www.udemy.com/terms",
+				new Contact("Nelio Alves", "udemy.com/user/nelio-alves", "nelio.cursos@gmail.com"),
+				"Permitido uso para estudantes", "https://www.udemy.com/terms", Collections.emptyList() // Vendor
+																										// Extensions
+		);
 	}
 	
+	private ResponseMessage simpleMessage(int code, String msg) {
+		return new ResponseMessageBuilder().code(code).message(msg).build();
+	}
 	
-	
+
+
 }
